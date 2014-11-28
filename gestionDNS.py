343d4003d14a5e3cf95c71d601628e684ae399df
fran <fran@targaryen>
2014-11-28 20:39:38 +0100

    no elimina

--- gestionDNS.py
+++ gestionDNS.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
-dns = open('/home/fran/dns', 'a')
+import os
+dns = open('/var/cache/bind/db.tronos', 'a')
 
 modo = sys.argv[1]
 
@@ -12,25 +12,29 @@ if modo == '-a':
 	alias = sys.argv[4]
 
 	if tipo == '-dir':
-	        dns.write(nombre+'      A       '+alias+'\n')
-
+		inv = open('/var/cache/bind/db.192','a')
+		dns.write(nombre+'    IN    A       '+alias+'\n')
+		inv.write(alias+'    IN    PTR    '+nombre+'.tronos.com.\n')
+		inv.close()
 	elif tipo == '-alias':
 		dns.write(nombre+'      CNAME   '+alias+'\n')
-	dns.close()
+dns.close()
 
 if modo == '-b':
+#solo elimina si la línea tiene una palabra
 	nombre = sys.argv[2]
 
-	dns = open("/home/fran/dns","r")
+	dns = open("/var/cache/bind/db.tronos","r")
 	lineas = dns.readlines()
 
 	dns.close()
 
-	dns = open("/home/fran/dns","w")
+	dns = open("/var/cache/bind/db.tronos","w")
 
 	for linea in lineas:
 		if linea != nombre+"\n":
 			dns.write(linea)
+os.system('service bind9 restart')
 
 dns.close()
 
